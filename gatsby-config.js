97ac44b300dbd8c25f36e718a950ea71554a336b
Petr Bulánek <petr.bulanek@superkoders.com>
2018-04-11 22:34:22 +0200

    Add .editorconfig

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Frontendisti.cz',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-  ],
-}
+	siteMetadata: {
+		title: 'Frontendisti.cz',
+	},
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		{
+			resolve: 'gatsby-plugin-netlify-cms',
+			options: {
+				modulePath: `${__dirname}/src/cms/cms.js`,
+			},
+		},
+	],
+};
